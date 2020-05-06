01f37983826bb57fba9f35c3fa3552e4f18eead7
Luca B <lucabulles@gmail.com>
2020-05-06 16:57:20 +0200

    Added DAl layer

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using DierenasielTongelreDAL.Queries;
 
 namespace DierenasielTongelre
 {
@@ -24,6 +25,8 @@ namespace DierenasielTongelre
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.Add(new ServiceDescriptor(typeof(AnimalQueries), new AnimalQueries(Configuration.GetConnectionString("DefaultConnection"))));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
